ccadab85a2f7704d85f902294b528e2a1131d6b2
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2016-08-25 15:20:12 +0800

    feature: top control buttons are now working
    The start, pause and clear button at the top panel are now functioning. The
    start button runs a setInterval that increments the generation count.
    Pressing the pause button halts the increment. Finally, the clear button
    stops the increment and resets it to zero.

--- src/index.js
+++ src/index.js
@@ -19,17 +19,18 @@ class Board extends React.Component {
     this.state = {
       numCol: 70,
       numRow: 50,
-      speed: 2
+      speed: 1000,
+      generation: 0
     };
   }
   render() {
     return (
       <div className='board'>
         <div className='control-top'>
-          <button id='run-btn' className='btn'>Run</button>
-          <button id='pause-btn' className='btn'>Pause</button>
-          <button id='clear-btn' className='btn'>Clear</button>
-          <span className='counter-label'>Generation: </span><span className='counter'>999999</span>
+          <button id='run-btn' className='btn' onClick={ () => this._startAnimation() }>Run</button>
+          <button id='pause-btn' className='btn' onClick={ () => this._pauseAnimation() }>Pause</button>
+          <button id='clear-btn' className='btn' onClick={ () => this._clearBoard() }>Clear</button>
+          <span className='counter-label'>Generation: </span><span className='counter'>{this.state.generation}</span>
         </div>
         <div className={'cell-container cell-container-' + this.state.numCol + 'x' + this.state.numRow}>
           {this._generateCells() }
@@ -37,9 +38,9 @@ class Board extends React.Component {
         <div className='control-bottom'>
           <div className='control-size'>
             <span className='control-label'>Board Size: </span>
-            <button id='size-50x30-btn' className='btn' onClick={(ev) => this._changeSize(ev)} value='50x30'>50x30</button>
-            <button id='size-70x50-btn' className='btn' onClick={(ev) => this._changeSize(ev)} value='70x50'>70x50</button>
-            <button id='size-100x80-btn' className='btn' onClick={(ev) => this._changeSize(ev)} value='100x80'>100x80</button>
+            <button id='size-50x30-btn' className='btn' onClick={(ev) => this._changeSize(ev) } value='50x30'>50x30</button>
+            <button id='size-70x50-btn' className='btn' onClick={(ev) => this._changeSize(ev) } value='70x50'>70x50</button>
+            <button id='size-100x80-btn' className='btn' onClick={(ev) => this._changeSize(ev) } value='100x80'>100x80</button>
           </div>
           <div className='control-speed'>
             <span className='control-label'>Sim Speed: </span>
@@ -62,6 +63,32 @@ class Board extends React.Component {
     }
     return cells;
   }
+  /**
+   * Start the animation by running a setInterval instance
+   */ 
+  _startAnimation() {
+    this.interval = setInterval(() => {
+        this.setState({
+          generation: this.state.generation + 1
+        });
+    }, this.speed);
+  }
+  /**
+   * Halts the animation by stopping the counter
+   */ 
+  _pauseAnimation() {
+    clearInterval(this.interval);
+  }
+  /**
+   * Clears the board and resets the counter to zero
+   * TODO: clearing the board
+   */ 
+  _clearBoard() {
+    this._pauseAnimation();
+    this.setState({
+      generation: 0
+    });
+  }
   _changeSize(ev) {
     const [numCol, numRow] = ev.target.value.split('x').map(val => parseInt(val));
     this.setState({
@@ -82,14 +109,14 @@ class Cell extends React.Component {
   }
   render() {
     return (
-      <div id={this.props.name} className={this.state.className} onClick={(ev) => this._onClick(ev)}>
+      <div id={this.props.name} className={this.state.className} onClick={(ev) => this._onClick(ev) }>
       </div>
     );
   }
   _onClick(ev) {
     var className = ev.target.className;
-    if(/\ new\-born/.test(className)) {
-      className = className.replace(/\ new\-born/,'');
+    if (/\ new\-born/.test(className)) {
+      className = className.replace(/\ new\-born/, '');
     } else {
       className += ' new-born';
     }
