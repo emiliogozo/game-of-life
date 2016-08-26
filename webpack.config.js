8bfc9e31e3d1f6db488e166354bc804cf113d107
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2016-08-27 03:45:16 +0800

    chore: Added babel and eslint configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
-var path = require('path');
-var srcPath = path.join(__dirname, 'src');
-var buildPath = path.join(__dirname, 'dist');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: path.join(srcPath, 'index.html'),
-    filename: 'index.html',
-    inject: 'body'
+const srcPath = path.join(__dirname, 'src');
+const buildPath = path.join(__dirname, 'dist');
+
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(srcPath, 'index.html'),
+  filename: 'index.html',
+  inject: 'body'
 });
 
 module.exports = {
-    devtool: 'source-map',
-    context: srcPath,
-    entry: path.join(srcPath, 'index.js'),
-    output: {
-        path: buildPath,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-                test: /\.scss$/,
-                include: /src/,
-                loaders: ['style', 'css', 'sass']
-            }
-        ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
+  devtool: 'source-map',
+  context: srcPath,
+  entry: path.join(srcPath, 'main.jsx'),
+  output: {
+    path: buildPath,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        include: /src/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
